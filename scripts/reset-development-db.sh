9d7511a7ff004c87e1d7b128474d73a7bdd9c0ab
Jakub Niewczas <niewczas.jakub@gmail.com>
2018-09-04 21:19:10 +0900

    rename CLEARDB_DATABASE_URL ->  EVENTS_DATABASE_URL env to be more generic

--- scripts/reset-development-db.sh
+++ scripts/reset-development-db.sh
@@ -2,7 +2,7 @@
 rm app/db/*.db
 (
 export HEROKU_RELEASE_VERSION=dev
-export CLEARDB_DATABASE_URL='sqlite://./app/db/events.db'
+export EVENTS_DATABASE_URL='sqlite://./app/db/events.db'
 export DATABASE_URL='sqlite://./app/db/state.db'
 
 ruby ./app/tasks/deploy_release_phase.rb
