43339bf2b5b2f8a7de5e0f1dc07323915613a72d
Jakub Niewczas <niewczas.jakub@gmail.com>
2018-09-22 23:35:38 +0900

    adjust test env setup to be able to keep spec files next to their targets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,16 @@ RSpec.configure do |config|
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
+
+require 'dry-container'
+require 'require_all'
+require './app/lib/auto_inject'
+
+APP_DEPENDENCIES = Dry::Container.new.tap do |c|
+end
+
+Import = AutoInject.new(APP_DEPENDENCIES)
+
+require_all(
+  Dir.glob('./app/lib/**/*.rb').reject { |fname| fname.include?('lib.rb') || fname.include?('spec.rb') },
+)
